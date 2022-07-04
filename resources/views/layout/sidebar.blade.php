154e1f41ddc2870bd1d17c60de0d3d3efa8afde3
Winda Kartika <windakartikaningsih@gmail.com>
2022-07-04 15:53:24 +0700

    Typo Sidebar BCA FINANCE

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -4,7 +4,7 @@
 <div class="left-sidenav">
     <ul class="metismenu left-sidenav-menu">
         <li class="<?= ($uri == 'transaksi' ? 'mm-active' : ''); ?>">
-            <a href="<?= route('getListTransaksi') ?>"><i class="ti-wallet"></i><span>List TransSaaksi</span></a>
+            <a href="<?= route('getListTransaksi') ?>"><i class="ti-wallet"></i><span>List Transaksi</span></a>
         </li>
         <li class="<?= ($uri == 'saldo' ? 'mm-active' : ''); ?>">
             <a href="<?= route('getListSaldo') ?>"><i class="ti-money"></i><span>List Saldo</span></a>
